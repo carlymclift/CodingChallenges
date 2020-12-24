d027b02f1f8ace5c035cbdddf1396432f24e70f6
Carly Clift <carlymclift@gmail.com>
2020-12-24 00:16:42 -0700

    Pass test for divisable nums

--- codewars/codewars.js
+++ codewars/codewars.js
@@ -1,7 +1,32 @@
 const kata8 = {
+  // multiply the two numbers given as arguments and 
+  // out the total
   multiply(a, b) {
     return a * b
-  }
+  },
+
+  /////////////////////////////////////////////////
+  //Complete the function which takes two arguments 
+  //and returns all numbers which are divisible by 
+  //the given divisor. First argument is an array 
+  //of numbers and the second is the divisor.
+
+  //My solution:
+  divisibleBy(numbers, divisor) {
+    let divisibleNum = numbers.reduce((acc, num) => {
+      if (num % divisor === 0) {
+        acc.push(num)
+      }
+      return acc
+    }, [])
+    return divisibleNum
+  },
+
+  //Better solution:
+//   divisibleBy(numbers, divisor) {
+//     return numbers.filter(n => n % divisor === 0)
+//   }
+  
 }
 
 module.exports = {
