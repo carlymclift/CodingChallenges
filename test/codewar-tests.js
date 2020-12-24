2a0d6d7e8aa5b0188a876ae79fa2fdecf4ebf8a5
Carly Clift <carlymclift@gmail.com>
2020-12-24 00:16:18 -0700

    Write test for finding divisable nums given num

--- test/codewar-tests.js
+++ test/codewar-tests.js
@@ -14,5 +14,13 @@ describe('Challenges', () => {
       expect(kata8.multiply(3,5)).to.deep.equal(15)
     })
 
+    it('Find numbers which are divisible by given number', () => {
+      expect(kata8.divisibleBy([1,2,3,4,5,6], 2)).to.deep.equal([2,4,6])
+      expect(kata8.divisibleBy([1,2,3,4,5,6], 3)).to.deep.equal([3,6])
+      expect(kata8.divisibleBy([0,1,2,3,4,5,6], 4)).to.deep.equal([0,4])
+      expect(kata8.divisibleBy([0], 4)).to.deep.equal([0])
+      expect(kata8.divisibleBy([1,3,5], 2)).to.deep.equal([])
+    })
+
   })
 })
